1f59fbdf873c5c974e3d8347459257491a996476
Maacchin <nitramagrec@gmail.com>
2025-03-10 14:29:43 -0300

    Create Comp_aceletrometro_dynaloggers.py

---
+++ Comp_aceletrometro_dynaloggers.py
@@ -0,0 +1,19 @@
+# Comparação Medições Acelerometro e dynaloggers
+
+# Carregar informação dos Dynaloggers e Acelerometro
+ 
+# Calcular o RMS de cada um
+
+# Plottar Cada Dynalogger no tempo
+
+# FFT de Tudo
+
+# Nivel de Vibração
+
+# Densidade Espectral Ruido Branco (PSD)
+
+# Desndidade Espectral dos Dynaloggers e Accelerometro
+
+# Plottar o espectro (Frequência)
+
+# Plottar a Desnsidade Espectral
