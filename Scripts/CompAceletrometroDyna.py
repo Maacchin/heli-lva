e094ddd4b9cf26f697a41fe615394cf0f5997b7a
Maacchin <nitramagrec@gmail.com>
2025-03-11 10:17:49 -0300

    Cálculo do RMS

--- Scripts/CompAceletrometroDyna.py
+++ Scripts/CompAceletrometroDyna.py
@@ -1,24 +1,44 @@
-# Comparação Medições Acelerometro e dynaloggers
+# Comparação de Medições Acelerometro e dynaloggers
 
 # %% Bibliotecas
 import os
 import pandas as pd
+import numpy as np
 
-# %% Muda para o CWD (Current Working directory) correto
+# %% Opções do Pandas
+
+# Para poder ver todas as colunas do DataFrame
+pd.set_option('display.max_columns', None) 
+
+# %% Funções
+def rms(array):
+    return(np.sqrt(np.mean(np.square(array))))
+
+# %% Mudar para o CWD (Current Working directory) correto
 path = "C:\\Users\MartinR\\Desktop\\Projetos\\heli-lva\\Scripts"
 os.chdir(path)
 os.listdir()
 
 # %% Carregar informação dos Dynaloggers e Acelerometro
-data_dyna1 = pd.read_csv('waveform_Pedal_090325-1326.csv', sep=';')
-data_dyna2 = pd.read_csv('waveform_Direito_090325-1417.csv', sep=';')
-data_dyna3 = pd.read_csv('waveform_Motor_090325-1423.csv', sep=';')
-data_dyna4 = pd.read_csv('waveform_Esquerdo_090325-1643.csv', sep=';')
+dyna1_data = pd.read_csv('waveform_Pedal_090325-1326.csv', sep=';')
+dyna2_data = pd.read_csv('waveform_Direito_090325-1417.csv', sep=';')
+dyna3_data = pd.read_csv('waveform_Motor_090325-1423.csv', sep=';')
+dyna4_data = pd.read_csv('waveform_Esquerdo_090325-1643.csv', sep=';')
 
-data_acc = pd.read_csv('accelerometer_356A45.csv', sep=';')
+acc_data = pd.read_csv('accelerometer_356A45.csv', sep=';', header=0)
+# Limpando colunas vazias
+acc_data = acc_data.loc[:, ~acc_data.columns.str.contains('^Unnamed')]
+# Removendo espaço em branco dos headers
+acc_data = acc_data.rename(columns=lambda x: x.strip())
 
 
 # %% Calcular o RMS de cada um
+dyna1_rms = rms(dyna1_data['Vertical'].to_numpy())
+dyna2_rms = rms(dyna2_data['Vertical'].to_numpy())
+dyna3_rms = rms(dyna3_data['Vertical'].to_numpy())
+dyna4_rms = rms(dyna4_data['Vertical'].to_numpy())
+acc_rms = rms(acc_data['Z'].to_numpy())
+
 
 # %% Plottar Cada Dynalogger no tempo
 
@@ -28,7 +48,7 @@ data_acc = pd.read_csv('accelerometer_356A45.csv', sep=';')
 
 # %% Densidade Espectral Ruido Branco (PSD)
 
-# %% Desndidade Espectral dos Dynaloggers e Accelerometro
+# %% Densidade Espectral dos Dynaloggers e Accelerometro
 
 # %% Plottar o espectro (Frequência)
 
